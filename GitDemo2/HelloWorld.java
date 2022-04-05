79aee14017c81ba737ae9f4ab7529a18828b39b1
Warade <kalyani.warade@capgemini.com>
2022-04-05 13:33:36 +0530

    HelloWorld.java file is added in GitDemo2

---
+++ GitDemo2/HelloWorld.java
@@ -0,0 +1,12 @@
+//1. Write a program to print “Hello World” (enclosed within double quotes) on the standard output. 
+
+package com.cg;
+
+public class HelloWorld 
+{
+	public static void main(String args[])
+	{
+		System.out.println("\"Hello World\"");
+	}
+
+}
